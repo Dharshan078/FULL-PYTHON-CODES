0669feb0c7bc3796132865156912be2c5840f836
Dharshan078 <103129616+Dharshan078@users.noreply.github.com>
2022-04-22 20:51:41 +0530

    ASSIGNMENT OPERATORS IN PYTHON

---
+++ ASSIGNMENT OPERATORS.py
@@ -0,0 +1,28 @@
+"""
+=   ASSIGNMENT
++=  ADDITION
+-=  SUBTRACTION
+*=  MULTIPLICATION
+/=  DIVISION
+%=  MODULUS (REMINDER OF DIVISON)
+//= FLOOR DIVISION
+**= EXPONENTIAL
+"""
+a=220
+print("a =  ",a)
+a+=10
+print("a += ",a)
+a-=10
+print("a -= ",a)
+a*=2
+print("a *= ",a)
+a/=2
+print("a /= ",a)
+a%=2
+print("a %= ",a)
+a//=2
+print("a //= ",a)
+
+b=4
+b**=2
+print("b **= ",b)
