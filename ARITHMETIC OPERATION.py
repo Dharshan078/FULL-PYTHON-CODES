c42836135f43ba5b8c74ad01010382f509fcd9c0
Dharshan078 <103129616+Dharshan078@users.noreply.github.com>
2022-04-22 20:37:43 +0530

    ARITHMETIC OPERATIONS SYNTAX

---
+++ ARITHMETIC OPERATION.py
@@ -0,0 +1,25 @@
+"""
+ARITHMETIC OPERATIONS IN PYTHON
++   ADDITION
+-   SUBTRACTION
+*   MULTIPLICATION
+/   DIVISION
+%   MODULUS
+//  FLOOR DIVISION
+**  EXPONENTIAL
+"""
+
+a=20
+b=10
+print("A =",a)
+print("B= ",b)
+print("ADITION OF A & B",a+b)
+print("SUBTRACTION OF A & B",a-b)
+print("MULTIPLICATION OF A & B",a*b)
+print("DIVISION OF A & B",a/b)
+print("REMINDER OF A % B",a%b)
+print("FLOOR DIVISON OF A % B",a//b)
+print("EXPONENTIAL OF A & B",a**b)
+
+
+
