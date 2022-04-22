fcb2420a49ac3b1de6a371debc694af670591311
Dharshan078 <103129616+Dharshan078@users.noreply.github.com>
2022-04-22 20:53:05 +0530

    COMPARISON OPERATORS IN PYTHON

---
+++ COMPARISON OPERATORS.py
@@ -0,0 +1,20 @@
+"""
+==  EQUAL
+!=  NOT EQUAL
+>   GREATER THAN
+<   LESSER THAN
+>=  GREATER THAN OR EQUAL TO
+<=  LESSER THAN OR EQUAL TO
+"""
+a=10
+b=20
+
+print("IS IT EQUAL",a==b)
+print("IS IT NOT EQUAL",a!=b)
+print("IS IT GREATER THAN",a>b)
+print("IS IT LESSER THAN",a<b)
+print("IS IT GREATER THAN OR EQUAL TO",a>=b)
+print("IS IT LESSER THAN OR EQUAL TO",a<=b)
+
+
+
