4e6e7644956d86ef2f3eb91f6e0c497af9abb112
Dharshan078 <103129616+Dharshan078@users.noreply.github.com>
2022-04-22 20:53:34 +0530

    LOGICAL OPERATORS IN PYTHON

---
+++ LOGICAL OPERATIONS.py
@@ -0,0 +1,13 @@
+"""
+LOGICAL OPERATIONS
+and  both two conditions should be true
+or   at least one condition should be true
+not  just inverts and operator
+"""
+a=5
+print( a<=10 and a>=20)
+print( a<=10 or a>=20)
+print(not(a<=10 and a>=20))
+
+
+
