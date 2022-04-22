fab37eef83bb0579d099b6e15351fcf707aa3368
Dharshan078 <103129616+Dharshan078@users.noreply.github.com>
2022-04-22 20:54:39 +0530

    STRING MAINPULATION AND SLICING OPERATORS

---
+++ STRING MANIPULATION.py
@@ -0,0 +1,15 @@
+a= "sivadharshan is a good boy"
+print(a.capitalize()) #TO CHANGE FIRST LETTER OF WORD IS CAPITAL
+print(a.isupper()) #TO CHECK IT CAPITAL OR NOT
+print(a.islower()) #TO CHECK IT IS SMALLER OR NOT
+print(a.find('a')) #TO FIND IT HAS a
+print(a.isalnum()) #TO CHECK IT HAS NUMBER AND LETTER
+print(a.isalpha()) #TO CHECK IT HAS ONLY LETTER
+print(a.isnumeric()) #TO CHECK IT HAS ONLY LETTER
+print(a.upper()) #TO CHECK IT HAS ONLY LETTER
+print(a.lower()) #TO CHECK IT HAS ONLY LETTER
+print(a.count('a')) #TO CHECK IT HAS ONLY LETTER
+print(a.replace('a','d')) #TO REPLACE 'A' WITH 'D'
+print(a.endswith("DHARSHAN")) #TO CHECK ITS END WITH
+print(a.endswith("boy")) #TO CHECK ITS END WITH
+print(a.title()) #TO PUT CAPITAL OF 1ST LETTER FOR EACH WORD
---
+++ STRINGS.py
@@ -0,0 +1,11 @@
+"""
+ P  Y  T  H  O  N
+ 0  1  2  3  4  5
+-6 -5 -4 -3 -2 -1
+"""
+a="Hello how are you"
+print(a[0:4])
+print(a[:4])
+print(a[3:])
+print(a[-6:-1])
+print(a[::-1])
