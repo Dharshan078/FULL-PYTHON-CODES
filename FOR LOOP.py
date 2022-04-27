d9438b7b232796194275ddee4db63e46f1ac7b9b
Sivadharshan V <103129616+Dharshan078@users.noreply.github.com>
2022-04-27 06:17:45 +0530

    FOR LOOP

---
+++ FOR LOOP.py
@@ -0,0 +1,15 @@
+"""
+FOR LOOP IN PYTHON
+ITS A EASIEST LOOP IN PYTHON, IT MOSTLY USES RANGE
+SYNTAX for i in range(start,stop,step):
+EX1: TO PRINT EVEN NUMBERS IN PYTHON USING FOR LOOP
+"""
+for i in range(2,10,2):
+    print(i)
+
+for i in range(10):
+    a=int(input("ENTER A NUMBER: "))
+    b=int(input("ENTER SECOND NUMBER: "))
+    print("THE ADDITION OF A & B",a+b)
+    print("THE SUBTRACTION OF A & B",a-b)
+    print("THE MULTIPLICATION OF A & B",a*b)
