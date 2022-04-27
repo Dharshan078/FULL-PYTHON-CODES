9e34e59279f0267fae86980fd016b4d6fd1c8c3c
Sivadharshan V <103129616+Dharshan078@users.noreply.github.com>
2022-04-27 06:18:12 +0530

    IF STATEMENT

---
+++ IF STATEMENT.py
@@ -0,0 +1,9 @@
+"""
+IF STATEMENT IN PYTHON
+if Statement is used for decision-making operations
+If the statement is true it will print "if CLAUSE" in output
+EX 1: TO PRINT THE GIVEN NUMBER IS EVEN
+"""
+n=int(input("ENTER A NUMBER : "))
+if n%2==0:
+    print("THE GIVEN NUMBER IS EVEN NUMBER")
