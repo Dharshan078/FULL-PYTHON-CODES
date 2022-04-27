e565f9fd56767ce097cbed664614396292d40659
Sivadharshan V <103129616+Dharshan078@users.noreply.github.com>
2022-04-27 06:17:56 +0530

    IF ELSE

---
+++ IF ELSE.py
@@ -0,0 +1,12 @@
+"""
+IF ELSE STATEMENT IN PYTHON
+IF ELSE Statement is used for decision-making operations
+It has two conditions if and else,
+If the statement is true it will print "if clause" in output, if it's not true it will print "else clause" in output
+EX 1: TO CHECK THE GIVEN NUMBER IS ODD OR EVEN
+"""
+n=int(input("ENTER A NUMBER : "))
+if n%2==0:
+    print(n,"IS EVEN NUMBER")
+else:
+    print(n,"IS ODD NUMBER")
